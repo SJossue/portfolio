e098649e3fdcfe853030107965d28ca2dd8d22ad
Jossue Sarango <junesarango@gmail.com>
2026-03-02 19:02:28 -0500

    refactor: extract car model and camera controls

--- src/components/features/scene/index.ts
+++ src/components/features/scene/index.ts
@@ -1,3 +1,5 @@
+export { CameraRig } from './CameraRig';
+export { CarRig } from './CarRig';
 export { SceneSkeleton } from './SceneSkeleton';
 export { useSceneState } from './useSceneState';
 export { HomeScene } from './HomeScene';
